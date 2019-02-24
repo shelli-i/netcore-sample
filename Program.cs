dd3844f25d46839c555be256c29ae736a67df980
shelli-i <47705974+shelli-i@users.noreply.github.com>
2019-02-23 20:14:08 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace netcore_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
